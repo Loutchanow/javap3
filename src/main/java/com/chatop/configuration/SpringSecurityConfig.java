69b8841fbcac97ecae9d4577f0e18ea833c0daaf
LHOTTE Lucien <lucien.lhotte@orange.com>
2025-05-10 15:47:55 +0200

    Ajout mecanique jwt

--- src/main/java/com/chatop/configuration/SpringSecurityConfig.java
+++ src/main/java/com/chatop/configuration/SpringSecurityConfig.java
@@ -1,39 +1,78 @@
 package com.chatop.configuration;
 
+
+import javax.crypto.spec.SecretKeySpec;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.nimbusds.jose.jwk.source.ImmutableSecret;
 
 @Configuration
-@EnableWebSecurity
 public class SpringSecurityConfig {
 
-    private static final String[] PUBLIC_ROUTES = {
-        "/api/auth/login",
-        "/api/auth/register"
-    };
+	
+    private static final String[] PUBLIC = {
+          "/api/auth/login",
+          "/api/auth/register",
+  };
 
+    @Value("${jwt.secret}")
+    private String jwtKey;
+    
+	
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers(PUBLIC_ROUTES).permitAll()
-                .anyRequest().authenticated()
-            )
-            .httpBasic(Customizer.withDefaults());  
-        return http.build();
+       return http
+          .csrf(csrf -> csrf.disable())
+          .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+          .authorizeHttpRequests(auth -> auth
+          .requestMatchers(PUBLIC).permitAll()
+          .anyRequest().authenticated()
+          )
+          .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+          .httpBasic(Customizer.withDefaults()).build();
     }
 
-    @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+	@Bean
+	public JwtEncoder jwtEncoder() {
+		return new NimbusJwtEncoder(new ImmutableSecret<>(this.jwtKey.getBytes()));
+	}
+
+	@Bean
+	public JwtDecoder jwtDecoder() {
+		SecretKeySpec secretKey = new SecretKeySpec(this.jwtKey.getBytes(), 0, this.jwtKey.getBytes().length, "RSA");
+		return NimbusJwtDecoder.withSecretKey(secretKey).macAlgorithm(MacAlgorithm.HS256).build();
+	}
+
+	@Bean
+	public UserDetailsService users() {
+		UserDetails user = User.builder().username("user").password(passwordEncoder().encode("password")).roles("USER")
+				.build();
+		return new InMemoryUserDetailsManager(user);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
 }
-
 
 //import com.openclassrooms.chatop.service.CustomUserDetailsService;
 //import org.springframework.context.annotation.Bean;
--- src/main/java/com/chatop/controllers/AuthController.java
+++ src/main/java/com/chatop/controllers/AuthController.java
@@ -1,13 +1,26 @@
 package com.chatop.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.chatop.service.JWTService;
+
+
+
+
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
+	
+	private JWTService jwtService;
+
+	public AuthController(JWTService jwtService) {
+		this.jwtService = jwtService;
+	}
 
     @PostMapping("/register")
     public String register() {
@@ -23,5 +36,11 @@ public class AuthController {
     public String me() {
         return "Me endpoint";
     }
+    
+    @PostMapping("/testjwt")
+	public String getToken(Authentication authentication) {
+		String token = jwtService.generateToken(authentication);
+		return token;
+	}
 }
 
