8d40a7044cb04a247f3ff02e4d4362d99ba1cda2
LHOTTE Lucien <lucien.lhotte@orange.com>
2025-06-11 11:14:31 +0200

    readm me, plus ajout registerdto

--- src/main/java/com/chatop/controllers/AuthController.java
+++ src/main/java/com/chatop/controllers/AuthController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.chatop.dto.LoginRequestDTO;
+import com.chatop.dto.RegisterDTO;
 import com.chatop.dto.TokenResponseDTO;
 import com.chatop.dto.UsersDTO;
 import com.chatop.model.Users;
@@ -38,14 +39,20 @@ public class AuthController {
 	public AuthController(JWTService jwtService) {
 		this.jwtService = jwtService;
 	}
-
 	@PostMapping("/register")
-	public TokenResponseDTO register(@RequestBody UsersDTO usersDTO) {
-	    String rawPassword = usersDTO.getPassword();
-	    usersDTO.setPassword(passwordEncoder.encode(rawPassword));
-	    usersService.save(usersDTO);
+	public TokenResponseDTO register(@RequestBody RegisterDTO registerDTO) {
+	    String rawPassword = registerDTO.getPassword();
+	    String encoded = passwordEncoder.encode(rawPassword);
+
+	    UsersDTO user = new UsersDTO();
+	    user.setEmail(registerDTO.getEmail());
+	    user.setName(registerDTO.getName());
+	    user.setPassword(encoded);
+
+	    usersService.save(user);
+
 	    Authentication authentication = authenticationManager.authenticate(
-	        new UsernamePasswordAuthenticationToken(usersDTO.getEmail(), rawPassword)
+	        new UsernamePasswordAuthenticationToken(user.getEmail(), rawPassword)
 	    );
 	    String token = jwtService.generateToken(authentication);
 	    return new TokenResponseDTO(token);
---
+++ src/main/java/com/chatop/dto/RegisterDTO.java
@@ -0,0 +1,13 @@
+package com.chatop.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+    @Getter
+	@Setter
+	public class RegisterDTO {
+	    private String name;
+	    private String email;
+	    private String password;
+	}
+
